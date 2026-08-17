c0d3c3606cd2959c57aedde01f60894819b36516
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-08-17 14:43:32 -0700

    13.8.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "13.0.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.7.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.8.0")
     ],
     targets: [
         .target(
@@ -28,8 +28,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppLovinMediationGoogleAdManagerAdapter",
-            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/googleadmanager-adapter/AppLovinMediationGoogleAdManagerAdapter-13.7.0.0.zip",
-            checksum: "6b22024284684eb9c34eff0e570ae1ccde2ec3393416c53e1f2e54db86068286"
+            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/googleadmanager-adapter/AppLovinMediationGoogleAdManagerAdapter-13.8.0.0.zip",
+            checksum: "0e24d90741f1e9b69e7187142323fd39a69450d195facdefbe772429107c886e"
         )
     ]
 )
